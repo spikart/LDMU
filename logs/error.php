70cc3c403876b49d762f48ba5ad49167c3cf944f
spikart <zyuralex@gmail.com>
2015-10-03 13:40:29 +0300

    joomla

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-10-03 09:52:26 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-10-03T09:52:26+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
