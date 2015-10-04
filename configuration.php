2ac882b3610c61c0ef0ab748823d0349c0b0e7f4
spikart <zyuralex@gmail.com>
2015-10-04 14:28:18 +0300

    add text

--- configuration.php
+++ configuration.php
@@ -5,7 +5,7 @@ class JConfig {
 	public $display_offline_message = '1';
 	public $offline_image = '';
 	public $sitename = 'Музичне училище ім. С.Людкевича';
-	public $editor = 'jce';
+	public $editor = 'tinymce';
 	public $captcha = '0';
 	public $list_limit = '20';
 	public $access = '1';
